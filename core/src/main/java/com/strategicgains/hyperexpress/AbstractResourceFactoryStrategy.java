5ebf1685868fc701075a39cdb9b9ad27a71db7b8
Todd Fredrich <tfredrich@gmail.com>
2015-02-05 16:36:19 -0700

    Added Template Method: addProperty(Resource, Field, Object) to facilitate sub-class extension.

--- core/src/main/java/com/strategicgains/hyperexpress/AbstractResourceFactoryStrategy.java
+++ core/src/main/java/com/strategicgains/hyperexpress/AbstractResourceFactoryStrategy.java
@@ -127,7 +127,7 @@ implements ResourceFactoryStrategy
 					
 					if (value != null)
 					{
-						to.addProperty(f.getName(), value);
+						addProperty(to, f, value);
 					}
 				}
 			}
@@ -139,6 +139,20 @@ implements ResourceFactoryStrategy
 
 		copyProperties0(type.getSuperclass(), from, to);
 	}
+
+	/**
+	 * Template method. Sub-classes can override to possibly handle annotations on the field, etc.
+	 * By default, this method simply adds the property to the resource using the value and the
+	 * given name of the field.
+	 * 
+	 * @param to the destination resource.
+	 * @param f the field to copy.
+	 * @param value the value of the field.
+	 */
+	protected void addProperty(Resource to, Field f, Object value)
+    {
+	    to.addProperty(f.getName(), value);
+    }
 
 	/**
 	 * Answers whether the Field should be copied. By default it ignores (answers 'false' to)
